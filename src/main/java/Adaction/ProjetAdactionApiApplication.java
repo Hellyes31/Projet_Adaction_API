8b5da556161a820f5fdebc71ad513998d7da0cf8
Hellyes31 <elies.auguet@gmail.com>
2025-09-29 15:13:17 +0200

    add: product.java update: SpringBoot Server wip: link db to neon

---
+++ src/main/java/Adaction/ProjetAdactionApiApplication.java
@@ -0,0 +1,14 @@
+package Adaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAdactionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAdactionApiApplication.class, args);
+	}
+
+}
+
