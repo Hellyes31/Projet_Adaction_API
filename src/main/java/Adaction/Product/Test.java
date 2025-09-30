fc67610ccb64d2da698e16ef62d18f84a494cc7f
Hellyes31 <elies.auguet@gmail.com>
2025-09-30 12:15:05 +0200

    add: class Test

---
+++ src/main/java/Adaction/Product/Test.java
@@ -0,0 +1,22 @@
+package Adaction.Product;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import java.math.BigDecimal;
+
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal test;
+}
